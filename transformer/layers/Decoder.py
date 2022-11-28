2457a3f8faca510be2e296def24eccb0797f1909
LimpingGabriel <benjamin.nysetvold@gmail.com>
2022-11-28 15:02:56 -0500

    Added decoder layers.

---
+++ transformer/layers/Decoder.py
@@ -0,0 +1,40 @@
+from keras.layers import Layer
+from keras.layers import Dropout
+from PositionalEmbedding import PositionalEmbedding
+from DecoderLayer import DecoderLayer
+
+class Decoder(Layer):
+    """description of class"""
+
+    def __init__(self, *, num_layers, d_model, num_heads, dff, vocab_size, dropout=0.1):
+        super(Decoder, self).__init__()
+
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.pos_embedding = PositionalEmbedding(
+            vocab_size=vocab_size,
+            d_model=d_model)
+        self.dropout = Dropout(dropout_rate)
+        self.dec_layers = [
+            DecoderLayer(
+                d_model=d_model,
+                num_heads=num_heads,
+                dff=dff,
+                dropout_rate=dropout_rate)
+            for _ in range(num_layers)]
+
+        self.last_attn_scores = None
+
+        def call(self, x):
+            x = self.pos_embedding(x)
+            x = self.dropout(x)
+            for i in range(self.num_layers):
+                x = self.dec_layers[i](x, context)
+
+            self.last_attn_scores = self.dec_layers[-1].last_attn_scores
+            return x
+
+
+
+
---
+++ transformer/layers/Encoder.py
@@ -0,0 +1,40 @@
+from keras.layers import Layer
+from keras.layers import Dropout
+from PositionalEmbedding import PositionalEmbedding
+from EncoderLayer import EncoderLayer
+
+class Encoder(Layer):
+    """description of class"""
+
+    def __init__(self, *, num_layers, d_model, num_heads, dff, vocab_size, dropout_rate=0.1):
+        super().__init__()
+        
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.pos_embedding = PositionalEmbedding(
+            vocab_size = vocab_size,
+            d_model = d_model)
+
+        self.enc_layers = [
+            EncoderLayer(d_model=d_model,
+                         num_heads=num_heads,
+                         dff=dff,
+                         dropout_rate=dropout_rate)
+            for _ in range(num_layers)]
+
+        self.dropout = Dropout(dropout_rate)
+
+
+        def call(self, x):
+            x = self.pos_embedding(x)
+            x = self.dropout(x)
+
+            for i in range(self.num_layers):
+                x = self.enc_layers[i](x)
+
+            return x
+
+
+
+
