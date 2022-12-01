3305cf8759a98e8c8631b29c5b4ed2a767c1e749
LimpingGabriel <benjamin.nysetvold@gmail.com>
2022-12-01 01:11:52 -0500

    Updated main.py to now be fully self-sufficient
    Can be run by itself.

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from Translator import *
-from ExportTranslator import *
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+import tensorflow_text
 import tensorflow as tf
 
-
-
 if __name__ == "__main__":
-    settings = GlobalSettings()
-    dataset = TextDataset("Shakescleare")
-
-
-    ex = tf.saved_model.load("transformer_batch_size-32__num_layers-6__d_model-128__dff-512__num_heads-8__dropout-0.3__epochs-50_")
+    ex = tf.saved_model.load("Models/transformer_batch_size-64__num_layers-4__d_model-128__dff-256__num_heads-8__dropout-0.3__epochs-200_")
 
     while True:
-        print("Output: {}".format(ex(tf.constant(input("Input: ")))))
+        print("Output: {}".format(ex(tf.constant(input("Input: "))).numpy().decode("utf-8")))
