67ef662142083f0b264b92c76913b3b70cdc308a
LimpingGabriel <benjamin.nysetvold@gmail.com>
2022-12-01 01:02:47 -0500

    Removed redundancies from GlobalSettings.py

--- GlobalSettings.py
+++ GlobalSettings.py
@@ -20,11 +20,3 @@ class GlobalSettings(object):
         # Hyperparameters
         self.MAX_TOKENS = 128
         self.BUFFER_SIZE = 20000
-        self.BATCH_SIZE = 16
-
-        self.NUM_EPOCHS = 300
-        self.TRANSFORMER_NUM_LAYERS = 4 #4
-        self.TRANSFORMER_D_MODEL = 256 # 128
-        self.TRANSFORMER_DFF = 1024 # 512
-        self.TRANSFORMER_NUM_HEADS = 8 # 8
-        self.TRANSFORMER_DROPOUT_RATE = 0.1 # 0.1
