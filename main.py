1d4a5cc1d8696e91f7b4ce67af4e7a5e5d8e7c58
LimpingGabriel <benjamin.nysetvold@gmail.com>
2022-12-01 01:13:26 -0500

    Removed tokenizer; not necessary to run. Compiled for training if necessary.

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow_text
 import tensorflow as tf
 
+MODEL_NAME = "Models/transformer_batch_size-64__num_layers-4__d_model-128__dff-256__num_heads-8__dropout-0.3__epochs-200_"
+
 if __name__ == "__main__":
-    ex = tf.saved_model.load("Models/transformer_batch_size-64__num_layers-4__d_model-128__dff-256__num_heads-8__dropout-0.3__epochs-200_")
+    ex = tf.saved_model.load(MODEL_NAME)
 
     while True:
         print("Output: {}".format(ex(tf.constant(input("Input: "))).numpy().decode("utf-8")))
