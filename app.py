7c2f8c058226ffb8704b7b78ad6c96d9a35a3124
LimpingGabriel <benjamin.nysetvold@gmail.com>
2022-11-30 23:10:27 -0500

    Temporary working files

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+import tensorflow as tf
+
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+tfmodel = tf.saved_model.load("transformer_batch_size-64__num_layers-4__d_model-128__dff-256__num_heads-8__dropout-0.3__epochs-200_")
+
+
+
 
 app = Flask(__name__)
 
-messages = [{"input" : "test_input",
-             "output": "test_output"
-             }]
+translations = []
 
-@app.route("/")
+@app.route("/", methods=("GET", "POST"))
 def index():
-    return render_template("index.html", messages=messages)
+    if request.method == "POST":
+        content = request.form["content"]
+
+    translations.append(
+        {"input": content,
+         "output": tfmodel(tf.constant(content.decode("utf-8"))).decode("utf-8")})
+
+    return render_template("index.html", translations=translation)
